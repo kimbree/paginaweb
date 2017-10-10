40140e81d7373795a4148563abb9cd8ac9d57dce
kimbree <pedromoreira1490@gmail.com>
2017-10-09 20:48:41 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head><title>Markar</title></head>
+<body>
+<?php
+echo "Somos Markar es una empresa de Marketing conocida a nivel mundial";
+echo "<br>";
+echo "Una empresa de Marketing argentina";
+echo "<br>";
+echo "Te daremos la mejor asesoria para que tu producto se venda";
+?>
+</body>
+</html>
